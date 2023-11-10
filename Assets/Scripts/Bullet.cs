c429e814d482e0e965799c3faaa1aee84c9acbb5
Jeremiah Rhoton <JeremiahHR133@gmail.com>
2023-11-09 18:00:50 -0600

    Add some more stuff idk

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float yLimit;
+    public GameObject soundGenerator;
+    public AudioClip collideSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shooting.IncrementBulletCount(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > yLimit)
+        {
+            Die();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Alien")
+        {
+            GameObject sg = Instantiate(soundGenerator);
+            AudioSource audioSource = sg.GetComponent<AudioSource>();
+            audioSource.clip = collideSound;
+            audioSource.Play();
+        }
+    }
+
+    void Die()
+    {
+        Shooting.IncrementBulletCount(-1);
+        Destroy(gameObject);
+    }
+}
